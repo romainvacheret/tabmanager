3675c67f798acf744565649c4750ff071f843279
Romain Vacheret <romain.vacheret@gmail.com>
2025-02-22 11:27:58 +0100

    feat(extension): add pages for actions

--- extentions/chrome/popup.js
+++ extentions/chrome/popup.js
@@ -1,26 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("listTabs").addEventListener("click", () => {
-        chrome.tabs.query({ highlighted: true}, (tabs) => {
-            let tableBody = document.querySelector("#tabsTable tbody");
-            tableBody.innerHTML = ""; 
+    const homePage = document.getElementById("homePage");
+    const storePage = document.getElementById("storePage");
+    const retrievePage = document.getElementById("retrievePage");
 
-            chrome.tabs.query({ active: true, currentWindow: true }, (activeTabs) => {
-                let activeTabId = activeTabs.length > 0 ? activeTabs[0].id : null;
+    const storeButton = document.getElementById("storeButton");
+    const retrieveButton = document.getElementById("retrieveButton");
+    const backButtons = document.querySelectorAll(".backButton");
+    const listTabsButton = document.getElementById("listTabs");
 
-                tabs.forEach((tab) => {
-                    let row = document.createElement("tr");
-                    let cell = document.createElement("td");
-                    cell.textContent = tab.url; 
+    const buttonListeners = [
+        [storeButton, storePage],
+        [retrieveButton, retrievePage],
+        [backButtons[0], homePage],
+        [backButtons[1], homePage],
+    ];
 
-                    if (tab.id === activeTabId) {
-                        cell.style.color = "blue";
-                        cell.style.fontWeight = "bold";
-                    }
+    const showPage = (page) => {
+        document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
+        page.classList.add("active");
+    }
 
-                    row.appendChild(cell);
-                    tableBody.appendChild(row);
-                });
-            });
+    buttonListeners.forEach(([button, page]) => button.addEventListener("click", () => showPage(page)));
+
+    const createTabTable = (tabs) => {
+        let tableBody = document.querySelector("#tabsTable tbody");
+        tableBody.innerHTML = ""; 
+
+        tabs.forEach(tab => {
+            let row = document.createElement("tr");
+            let cell = document.createElement("td");
+            cell.textContent = tab.title; 
+
+            row.appendChild(cell);
+            tableBody.appendChild(row);
         });
-    });
+    };
+
+    const listTabs = () => chrome.tabs.query(
+        // We only want the selected tabs from the current window
+        // If currentWindow is false, would also take the active tab 
+        // from other windows
+        { highlighted: true, currentWindow: true },
+        tabs => createTabTable(tabs)
+    );
+
+    listTabsButton.addEventListener("click", listTabs);
 });
